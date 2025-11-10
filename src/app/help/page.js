61fa75145738504c0414d7d5e828f93fd1acccca
Madelyn H <madelynghuang@gmail.com>
2025-11-10 12:58:46 -0500

    Finished Help page

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 export default function Page() {
     return (
-        <></>
+        <>
+            <Image src="/calculator-help.png" alt="blah" width={901} height={618} style={{
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto"
+            }} />
+        </>
     );
 }
