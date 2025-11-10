d35bfc019544e6e0ec9f2ed7e6bc3a8a2129fb12
Madelyn H <madelynghuang@gmail.com>
2025-11-10 13:09:47 -0500

    Fix image link

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Page() {
     return (
         <>
-            <Image src="/calculator-help.png" alt="blah" width={901} height={618} style={{
+            <Image src="/calculator/calculator-help.png" alt="blah" width={901} height={618} style={{
                 display: "block",
                 marginLeft: "auto",
                 marginRight: "auto"
